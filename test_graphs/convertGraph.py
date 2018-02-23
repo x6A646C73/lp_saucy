ab614169c3dbb84d44359f774636165d36f326cf
Jonathan Schrock <jschrock@Newton.local>
2018-02-23 14:06:44 -0500

    Fixed broken graph files

--- test_graphs/convertGraph.py
+++ test_graphs/convertGraph.py
@@ -8,15 +8,18 @@ if __name__ == "__main__":
         if data[0] == "c":
             continue
         elif data[0] == "p":
-            V = data[2]
-            E = data[3]
+            V = int(data[2])
+            E = int(data[3])
         elif data[0] == "e":
-            edges.append( (int(data[1])-1, int(data[2])-1, 1) )
+            temp1 = (int(data[1])-1, int(data[2])-1)
+            temp2 = (int(data[2])-1, int(data[1])-1)
+            if temp1 not in edges and temp2 not in edges:
+                edges.append( (int(data[1])-1, int(data[2])-1) )
     f.close()
 
     f = open( sys.argv[1]+".amorph", "w" )
-    f.write( V+" "+E+" 1\n" )
-    for i in xrange( int(V) ):
+    f.write( str(V)+" "+str(len(edges))+" 1\n" )
+    for i in xrange( V ):
         f.write( str(i)+" 0\n" )
     for e in edges:
         f.write( str(e[0])+" "+str(e[1])+" 1\n")
